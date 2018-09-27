71959bd5db03472db8ce02adeef54410539a867e
wada811 <at.wada811@gmail.com>
2018-09-27 14:09:17 +0900

    Write my code

--- kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -2,23 +2,39 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
 
 /**
  * Fragment representing the login screen for Shrine.
  */
 class LoginFragment : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-
-        // Snippet from "Navigate to the next Fragment" section goes here.
-
-        return inflater.inflate(R.layout.shr_login_fragment, container, false)
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+        view.next_button.setOnClickListener {
+            if (isPasswordValid(password_edit_text.text)) {
+                password_edit_text.error = null
+                // Snippet from "Navigate to the next Fragment" section goes here.
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            } else {
+                password_edit_text.error = getString(R.string.shr_error_password)
+            }
+        }
+        view.password_edit_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edit_text.text)) {
+                password_edit_text.error = null
+            }
+            false
+        }
+        return view
     }
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
+    private fun isPasswordValid(text: Editable?): Boolean = text?.let { it.length >= 8 } ?: false
 }
